963c6118453c831433ac1fb8f8043f6f35b35db4
Catherine Noel <catherinemnoel@gmail.com>
2017-08-30 13:16:49 -0400

    update

--- key.js
+++ key.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'jYo191bZSxzeLhSIXze2Vnya6',
+  consumer_secret: 'lNx7f4O94qw4z8Ld55bhnbuFZnV5Sl93hxghu4e3TIGmAaqSN3',
+  access_token_key: '28267828-LoJkIXqH1cFiBL9TWkJ8UmE1lzjap1rm7TtClnJie',
+  access_token_secret: 'qC2MkqBbmoE3wVe0VWqBaV45rl7GFdvUeHYlSo23dJIky',
 }
---
+++ liri.js
@@ -0,0 +1,1 @@
+var Key = require("./key.js")
